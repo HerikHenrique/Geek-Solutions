8a457975f4b81de23fce1ecb960d46de102eb280
HerikHenrique <herikcontato@gmail.com>
2025-05-21 22:32:47 -0300

    Modelagem da Classe Cliente

--- todos/models.py
+++ todos/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 
+class Cliente(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    rg = models.CharField(max_length=14, unique=True)
+    cpf = models.CharField(max_length=14, unique=True)
+    data_cadastro = models.DateField(auto_now_add=True)
+    endereco = models.CharField(max_length=255)
+    telefone = models.CharField(max_length=15)
+    email_cliente = models.EmailField(max_length=100, unique=True)
