71eb655aa239f06daa7b2804941331e78268bf7c
HerikHenrique <herikcontato@gmail.com>
2025-05-22 22:28:59 -0300

    Atualiza dependências do React e corrige compatibilidade do frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <nav>
+          <Link to="/">Home</Link> |{' '}
+          <Link to="/about">Sobre</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/About.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About: React.FC = () => (
+  <div>
+    <h1>Sobre</h1>
+    <p>Esta é a página Sobre do seu projeto React!</p>
+  </div>
+);
+
+export default About;
